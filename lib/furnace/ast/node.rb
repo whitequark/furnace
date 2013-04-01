whitequark/furnace

commit 2f29525aa563a0d3a626353d8428af558a5e8df2

Better AST::Node#dup.

lib/furnace/ast/node.rb
@@ -78,7 +78,14 @@ module Furnace::AST
     end
     protected :assign_properties
 
-    protected :dup
+    # Nodes are already frozen, so there is no harm in returning the
+    # current node as opposed to initializing from scratch and freezing
+    # another one.
+    #
+    # @return self
+    def dup
+      self
+    end
 
     # Returns a new instance of Node where non-nil arguments replace the
     # corresponding fields of `self`.

commit e65fdf47741670709d236d68a321279853e01f6e

Fix a bug introduced in 2f29525.

lib/furnace/ast/node.rb
@@ -78,6 +78,9 @@ module Furnace::AST
     end
     protected :assign_properties
 
+    alias   :original_dup :dup
+    private :original_dup
+
     # Nodes are already frozen, so there is no harm in returning the
     # current node as opposed to initializing from scratch and freezing
     # another one.
@@ -110,7 +113,7 @@ module Furnace::AST
           properties.nil?
         self
       else
-        dup.send :initialize, new_type, new_children, new_properties
+        original_dup.send :initialize, new_type, new_children, new_properties
       end
     end
 

test/test_ast.rb
@@ -26,8 +26,8 @@ describe AST::Node do
     @node.children.frozen?.should.be.true
   end
 
-  it 'should not allow duping' do
-    -> { @node.dup }.should.raise NoMethodError
+  it 'should return self when duping' do
+    @node.dup.should.equal? @node
   end
 
   it 'should return an updated node, but only if needed' do